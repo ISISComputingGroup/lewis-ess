5e7518cdff46a4cae34a93a8c961d30e7da6b02e
Michael Wedel <michael.wedel@esss.se>
2016-04-29 11:54:53 +0200

    Added license
    The project is now GPL-3

--- adapters/epics.py
+++ adapters/epics.py
@@ -1,9 +1,25 @@
+#  -*- coding: utf-8 -*-
+# *********************************************************************
+# plankton - a library for creating hardware device simulators
+# Copyright (C) 2016 European Spallation Source ERIC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *********************************************************************
+
 from datetime import datetime
-
 from pcaspy import Driver, SimpleServer
-
 from simulation.core import CanProcess
-from simulation import SimulatedChopper
 
 
 class PropertyExposingDriver(CanProcess, Driver):
