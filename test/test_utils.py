123f1b42dc36b214430343bbfd3711359f8db455
Michael Wedel <michael.wedel@esss.se>
2016-06-29 17:23:32 +0200

    Test for ExtractModuleName

--- test/test_utils.py
+++ test/test_utils.py
@@ -44,3 +44,60 @@ class TestDictStrictUpdate(unittest.TestCase):
         update_dict = {51: 3, 2: 43}
 
         self.assertRaises(RuntimeError, dict_strict_update, base_dict, update_dict)
+
+
+from core.utils import extract_module_name
+import os, shutil
+import tempfile
+
+
+def generate_fake_package_structure():
+    _tmp_package = tempfile.mkdtemp()
+    _tmp_dir = tempfile.gettempdir()
+
+    files = {k: os.path.join(_tmp_package, v) for k, v in dict(
+        valid='some_file.py',
+        invalid_ext='some_other_file.pyc',
+        invalid_name='_some_invalid_file.py',
+    ).iteritems()}
+
+    for abs_file_name in files.values():
+        with open(abs_file_name, mode='w'):
+            pass
+
+    dirs = {k: os.path.join(_tmp_package, v) for k, v in dict(
+        valid='some_dir',
+        empty='empty_dir',
+        invalid='_invalid',
+    ).iteritems()}
+
+    for abs_dir_name in dirs.values():
+        os.mkdir(abs_dir_name)
+
+    with open(os.path.join(_tmp_package, 'some_dir', '__init__.py'), 'w'):
+        pass
+
+    return _tmp_package, _tmp_dir, files, dirs, ['some_file', 'some_dir']
+
+
+class TestExtractModuleName(unittest.TestCase):
+    def setUp(self):
+        self._tmp_package, tmp_dir, self._files, self._dirs, expected = generate_fake_package_structure()
+
+    def tearDown(self):
+        shutil.rmtree(self._tmp_package)
+
+    def test_directory_basename_is_returned(self):
+        self.assertEqual(extract_module_name(self._dirs['valid']), 'some_dir')
+
+    def test_directory_invalid_name(self):
+        self.assertEqual(extract_module_name(self._dirs['invalid']), None)
+
+    def test_file_invalid_name(self):
+        self.assertEqual(extract_module_name(self._files['invalid_name']), None)
+
+    def test_file_invalid_extension(self):
+        self.assertEqual(extract_module_name(self._files['invalid_ext']), None)
+
+    def test_file_basename_without_extension(self):
+        self.assertEqual(extract_module_name(self._files['valid']), 'some_file')
