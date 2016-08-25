3b61bc152f43bcbd993e77112f3a409e260ba9eb
Michael Hart <michael.hart@stfc.ac.uk>
2016-08-25 11:31:31 +0100

    Added more tests and corrected some comments

--- test/test_SimulatedLinkamT95.py
+++ test/test_SimulatedLinkamT95.py
@@ -68,18 +68,19 @@ class TestSimulatedLinkamT95(unittest.TestCase):
         # Heat for almost a minute but not quite
         linkam.process(59.5)
         status_bytes = linkam.getStatus()
-        self.assertNotEqual(status_bytes[6:10], '01b8')  # Temp != 44.0 C
+        self.assertEqual(status_bytes[0], '\x10')           # Heating status set
+        self.assertNotEqual(status_bytes[6:10], '01b8')     # Temp != 44.0 C
 
         # Finish off heating
         linkam.process(0.5)
         status_bytes = linkam.getStatus()
-        self.assertEqual(status_bytes[6:10], '01b8')  # Temp == 44.0 C
+        self.assertEqual(status_bytes[6:10], '01b8')    # Temp == 44.0 C
 
         # Should hold now, so temperature should not change
         linkam.process(10)
         status_bytes = linkam.getStatus()
-        self.assertEqual(status_bytes[0], '\x30')  # Auto-holding at limit
-        self.assertEqual(status_bytes[6:10], '01b8')  # Temp == 44.0 C
+        self.assertEqual(status_bytes[0], '\x30')       # Auto-holding at limit
+        self.assertEqual(status_bytes[6:10], '01b8')    # Temp == 44.0 C
 
     def test_simple_cool(self):
         linkam = SimulatedLinkamT95()
@@ -95,30 +96,122 @@ class TestSimulatedLinkamT95(unittest.TestCase):
         linkam.start()
         linkam.process(0)
 
-        # Heat for almost a minute but not quite
+        # Cool for almost a minute but not quite
         linkam.process(59.5)
         status_bytes = linkam.getStatus()
-        self.assertNotEqual(status_bytes[6:10], '0028')  # Temp != 4.0 C
+        self.assertEqual(status_bytes[0], '\x20')           # Cooling status set
+        self.assertNotEqual(status_bytes[6:10], '0028')     # Temp != 4.0 C
 
-        # Finish off heating
+        # Finish off cooling
         linkam.process(0.5)
         status_bytes = linkam.getStatus()
-        self.assertEqual(status_bytes[6:10], '0028')  # Temp == 4.0 C
+        self.assertEqual(status_bytes[6:10], '0028')    # Temp == 4.0 C
 
         # Should hold now, so temperature should not change
         linkam.process(10)
         status_bytes = linkam.getStatus()
-        self.assertEqual(status_bytes[0], '\x30')  # Auto-holding at limit
-        self.assertEqual(status_bytes[6:10], '0028')  # Temp == 4.0 C
+        self.assertEqual(status_bytes[0], '\x30')       # Auto-holding at limit
+        self.assertEqual(status_bytes[6:10], '0028')    # Temp == 4.0 C
+
+    def test_error_flag_overcool(self):
+        linkam = SimulatedLinkamT95()
+        linkam.process(0)  # Initialize
+
+        # Issue T command to get into stopped state
+        linkam.getStatus()
+        linkam.process(0)
+
+        # Ensure flag is not set
+        status_bytes = linkam.getStatus()
+        self.assertFalse(ord(status_bytes[1]) & 0x01)
+
+        # Set up to cool from 24.0 C to 4.0 C at 51.00 C/min
+        linkam.setRate('5100')
+        linkam.setLimit('40')
+        linkam.start()
+        linkam.process(0)
+
+        # Ensure flag is set after running a bit
+        linkam.process(0.1)
+        status_bytes = linkam.getStatus()
+        self.assertTrue(ord(status_bytes[1]) & 0x01)
 
     def test_stop_command(self):
-        pass
+        linkam = SimulatedLinkamT95()
+        linkam.process(0)  # Initialize
+
+        # Issue T command to get into stopped state
+        linkam.getStatus()
+        linkam.process(0)
+
+        # Set up to heat from 24.0 C to 44.0 C at 20.00 C/min
+        linkam.setRate('2000')
+        linkam.setLimit('440')
+        linkam.start()
+        linkam.process(0)
+
+        # Process for some time and then stop
+        linkam.process(10)
+        linkam.stop()
+        linkam.process(0)
+
+        # Ensure status byte reports stopped
+        status_bytes = linkam.getStatus()
+        self.assertEqual(ord(status_bytes[0]),  0x01)
 
     def test_hold_and_resume(self):
-        pass
+        linkam = SimulatedLinkamT95()
+        linkam.process(0)  # Initialize
 
-    def test_hold_while_at_limit(self):
-        pass
+        # Issue T command to get into stopped state
+        linkam.getStatus()
+        linkam.process(0)
+
+        # Set up to cool from 24.0 C to 4.0 C at 20.00 C/min
+        linkam.setRate('2000')
+        linkam.setLimit('40')
+        linkam.start()
+        linkam.process(0)
+
+        # Cool for a while
+        linkam.process(30)
+        status_bytes = linkam.getStatus()
+        self.assertEqual(status_bytes[0], '\x20')           # Cooling status set
+        self.assertNotEqual(status_bytes[6:10], '0028')     # Temp != 4.0 C
+
+        # Hold for a while
+        linkam.hold()
+        linkam.process(30)
+        status_bytes = linkam.getStatus()
+        self.assertEqual(status_bytes[0], '\x50')           # Manually holding
+        self.assertNotEqual(status_bytes[6:10], '0028')     # Temp != 4.0 C
+
+        # Finish cooling
+        linkam.cool()
+        linkam.process(30)
+        status_bytes = linkam.getStatus()
+        self.assertEqual(status_bytes[6:10], '0028')  # Temp == 4.0 C
+
+        # Make sure transitions to auto-holding
+        linkam.process(0)
+        status_bytes = linkam.getStatus()
+        self.assertEqual(status_bytes[0], '\x30')  # Auto-holding at limit
 
     def test_pump_command(self):
-        pass
+        linkam = SimulatedLinkamT95()
+        linkam.process(0)  # Initialize
+
+        # Issue T command to get into stopped state
+        linkam.getStatus()
+        linkam.process(0)
+
+        # Since the pump feature is not fully implemented, we can only make sure all valid input is accepted
+        linkam.pumpCommand('m0')    # Manual
+        linkam.process(0)
+
+        for c in "0123456789:;<=>?@ABCDEFGHIJKLMN":
+            linkam.pumpCommand(c)   # Various speeds (characters mean speeds 0 - 30)
+            linkam.process(0)
+
+        linkam.pumpCommand('a0')    # Auto
+        linkam.process(0)
