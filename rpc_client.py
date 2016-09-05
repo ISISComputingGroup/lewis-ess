9c335f4382c6dd18d5f20b9f1dd31536c3091492
Michael Wedel <michael.wedel@esss.se>
2016-09-05 15:50:37 +0200

    Proof of concept JSON-RPC

---
+++ rpc_client.py
@@ -0,0 +1,108 @@
+import zmq
+import types
+
+
+class ZMQJSONRPCServerException(Exception):
+    def __init__(self, type, message):
+        self.type = type
+        self.message = message
+
+    def __str__(self):
+        return 'Exception on server side of type \'{}\': \'{}\''.format(self.type, self.message)
+
+
+class ZMQJSONRPCProtocolException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+def json_rpc_call(socket, id, method, args=[]):
+    socket.send_json(
+        {'method': method,
+         'params': args,
+         'jsonrpc': '2.0',
+         'id': id
+         })
+
+    return socket.recv_json()
+
+class ZMQJSONRPCObjectProxyBase(object):
+    _remote_getters = set()
+    _remote_setters = set()
+
+    def __init__(self, socket, methods=[]):
+        self._id = 0
+        self.socket = socket
+        self._morph_into(methods)
+
+    def _make_request(self, method, args=[]):
+        self._id += 1
+
+        response = json_rpc_call(self.socket, self._id, method, args)
+
+        if 'result' in response:
+            return response['result']
+
+        if 'error' in response:
+            if 'data' in response['error']:
+                raise ZMQJSONRPCServerException(response['error']['data']['type'], response['error']['data']['message'])
+            else:
+                raise ZMQJSONRPCProtocolException(response['error']['message'])
+
+    def _morph_into(self, methods):
+        for method in [str(m) for m in methods]:
+            if '.set' in method:
+                self._remote_setters.add(method)
+            elif '.get' in method:
+                self._remote_getters.add(method)
+            elif not method.startswith('.'):
+                setattr(self, method, types.MethodType(self._create_wrapper_method(method), self))
+
+    def _create_wrapper_method(self, method_name):
+        def method_wrapper(obj, *args):
+            return obj._make_request(method_name, args)
+
+        return method_wrapper
+
+    def __getattribute__(self, item):
+        try:
+            return super(ZMQJSONRPCObjectProxyBase, self).__getattribute__(item)
+        except AttributeError:
+            if '{}.get'.format(item) in self._remote_getters:
+                return self._make_request('{}.get'.format(item))
+            raise
+
+    def __setattr__(self, key, value):
+        if '{}.set'.format(key) in self._remote_setters:
+            return self._make_request('{}.set'.format(key), [value,])
+
+        return super(ZMQJSONRPCObjectProxyBase, self).__setattr__(key, value)
+
+def ZMQJSONRPCObjectProxy(server='127.0.0.1', port='10000'):
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect('tcp://{0}:{1}'.format(server, port))
+
+    api = json_rpc_call(socket, 0, '.api')
+
+    if 'result' in api:
+        name = str(api['result']['class'])
+        methods = api['result']['methods']
+
+        return type(name, (ZMQJSONRPCObjectProxyBase,), {})(socket, methods)
+
+
+s = ZMQJSONRPCObjectProxy()
+
+
+print(s.a)
+s.a = 10
+print(s.a)
+s.doIt()
+print(s.a)
+s.doItNow(23)
+print(s.b)
+print(s.a)
+print(type(s))
---
+++ rpc_test.py
@@ -0,0 +1,137 @@
+class Stuff(object):
+    def __init__(self):
+        self._a = 0
+        self._b = (0, 0)
+
+    @property
+    def a(self):
+        return self._a
+
+    @a.setter
+    def a(self, value):
+        if value < 0:
+            raise ValueError('No values < 0!')
+
+        self._a = value
+
+    @property
+    def b(self):
+        return self._b
+
+    @b.setter
+    def b(self, one):
+        self._b = one
+
+    def doIt(self):
+        self._a = 42
+
+    def doItNow(self, foo, bar=34):
+        self._a = foo
+        self._b = (self._b[0], bar)
+
+
+class RPCObjectWrapper(object):
+    def __init__(self, object, methods=()):
+        self._object = object
+        self._object_type_dict = type(self._object).__dict__
+
+        self._method_map = {}
+
+        if not methods:
+            methods = [prop for prop in self._object_type_dict.keys() if not prop.startswith('_')]
+
+        for method in methods:
+            func_specs = self._create_method_wrappers(method)
+
+            for name, func in func_specs:
+                self._method_map[name] = func
+
+        self._method_map['.api'] = self.get_api
+
+    def _create_method_wrappers(self, method):
+        method_object = self._object_type_dict.get(method)
+
+        func_specs = []
+
+        if isinstance(method_object, property):
+            if method_object.fget:
+                def getter():
+                    return getattr(self._object, method)
+
+                func_specs.append(('{}.get'.format(method), getter,))
+
+            if method_object.fset:
+                def setter(arg):
+                    return setattr(self._object, method, arg)
+
+                func_specs.append(('{}.set'.format(method), setter,))
+
+        elif callable(method_object):
+            def method_wrapper(*args, **kwargs):
+                return getattr(self._object, method)(*args, **kwargs)
+
+            func_specs.append((method, method_wrapper,))
+
+        return func_specs
+
+    def get_api(self):
+        return {'class': type(self._object).__name__, 'methods': list(self._method_map.keys())}
+
+    def __call__(self, method, *args, **kwargs):
+        method = self._method_map.get(method)
+
+        if callable(method):
+            return method(*args, **kwargs)
+
+    def __getitem__(self, item):
+        return self._method_map[item]
+
+    def __setitem__(self, key, value):
+        self._method_map[key] = value
+
+    def __delitem__(self, key):
+        del self._method_map[key]
+
+    def __len__(self):
+        return len(self._method_map)
+
+    def __iter__(self):
+        return iter(self._method_map)
+
+import zmq
+from jsonrpc import JSONRPCResponseManager
+from time import sleep
+
+class ZMQJSONRPCServer(object):
+    def __init__(self, wrapper, host='127.0.0.1', port='10000'):
+        self.wrapper = wrapper
+        self.host = host
+        self.port = port
+
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REP)
+        uri = 'tcp://{0}:{1}'.format(self.host, self.port)
+        self.socket.bind(uri)
+
+    def process(self):
+        try:
+            request = self.socket.recv_unicode(flags=zmq.NOBLOCK)
+            print('Request: ', request)
+            response = JSONRPCResponseManager.handle(request, self.wrapper)
+            print('Sending response: ', response.json)
+            self.socket.send_unicode(response.json)
+        except zmq.Again:
+            pass
+
+
+class Bla(Stuff):
+    pass
+
+s = Bla()
+e = RPCObjectWrapper(s)
+
+server = ZMQJSONRPCServer(e)
+
+while True:
+    server.process()
+    sleep(0.1)
