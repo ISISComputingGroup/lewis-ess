9e8fa927d59e5315a8afa2aa9b13b4a1f14f35bf
Michael Wedel <michael.wedel@esss.se>
2016-10-07 18:25:12 +0200

    Added another even smaller example

---
+++ devices/example/__init__.py
@@ -0,0 +1,25 @@
+from adapters.stream import StreamAdapter, Cmd
+from core.processor import CanProcess
+
+
+class VerySimpleDevice(CanProcess):
+    param = 10
+
+
+class VerySimpleAdapter(StreamAdapter):
+    commands = {
+        Cmd('get_param', '^P$'),
+        Cmd('set_param', '^P=(.+)$'),
+    }
+
+    in_terminator = '\r\n'
+    out_terminator = '\r\n'
+
+    def get_param(self):
+        return self._device.param
+
+    def set_param(self, new_param):
+        self._device.param = new_param
+
+    def handle_error(self, request, error):
+        return 'An error occurred: ' + repr(error)
