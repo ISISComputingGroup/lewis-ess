5e2812f0f517fdf30c3370e75203e3c62437d9b1
Michael Wedel <michael.wedel@esss.se>
2016-10-14 10:18:05 +0200

    Remove context, rename method to fit naming convention

--- core/__init__.py
+++ core/__init__.py
@@ -17,5 +17,5 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # *********************************************************************
 
-from .statemachine import StateMachine, State, Transition, Context
+from .statemachine import StateMachine, State, Transition
 from .processor import CanProcess, CanProcessComposite
