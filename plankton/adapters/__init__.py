e8d6b1a6139325d328b5ee5fddd0b0fe90f59238
Michael Wedel <michael.wedel@esss.se>
2016-10-26 11:00:03 +0200

    Actually add files

---
+++ plankton/adapters/__init__.py
@@ -0,0 +1,158 @@
+#  -*- coding: utf-8 -*-
+# *********************************************************************
+# plankton - a library for creating hardware device simulators
+# Copyright (C) 2016 European Spallation Source ERIC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *********************************************************************
+
+import importlib
+
+
+class Adapter(object):
+    protocol = None
+
+    def __init__(self, device, arguments=None):
+        super(Adapter, self).__init__()
+        self._device = device
+
+    def start_server(self):
+        pass
+
+    def handle(self, cycle_delay=0.1):
+        pass
+
+
+def is_adapter(obj):
+    try:
+        return issubclass(obj, Adapter) and not obj.__module__.startswith('plankton.adapters')
+    except TypeError:
+        return False
+
+
+def get_available_adapters(device_name, device_package):
+    """
+    This helper function returns a dictionary with name/type pairs. It imports the module
+    device_package.device_name.adapters and puts those members of the module that inherit
+    from Adapter into the dictionary.
+
+    :param device_name: Device name for which to get the adapters.
+    :param device_package: Name of the package where devices are defined.
+    :return: Dictionary of name/type pairs for available adapters for that device.
+    """
+    adapters = dict()
+
+    try:
+        adapter_module = importlib.import_module(
+            '{}.{}.{}'.format(device_package, device_name, 'interfaces'))
+        module_members = {name: getattr(adapter_module, name) for name in dir(adapter_module)}
+
+        for name, member in module_members.items():
+            if is_adapter(member):
+                adapters[name] = member
+    except ImportError:
+        pass
+
+    device_module = importlib.import_module('{}.{}'.format(device_package, device_name))
+
+    for member in dir(device_module):
+        member_object = getattr(device_module, member)
+
+        if is_adapter(member_object):
+            adapters[member] = member_object
+
+    return adapters
+
+
+def import_adapter(device_name, protocol_name, device_package='devices'):
+    """
+    This function tries to import an adapter for the given device that implements
+    the requested protocol. If no adapter for that protocol exists, an exception
+    is raised. If protocol name is None, the function returns an
+    unspecified adapter. If no adapters are found at all, an error is raised.
+
+    :param device_name: Name of device for which an adapter is requested.
+    :param protocol_name: Requested protocol implemented by adapter.
+    :param device_package: Name of the package where devices are defined.
+    :return: Adapter class that implements requested protocol for the specified device.
+    """
+    available_adapters = get_available_adapters(device_name, device_package)
+
+    if not protocol_name:
+        return list(available_adapters.values())[0]
+
+    for adapter in available_adapters.values():
+        if adapter.protocol == protocol_name:
+            return adapter
+
+    raise RuntimeError(
+        'No suitable adapter found for device \'{}\' '
+        'and protocol \'{}\'.'.format(device_name, protocol_name))
+
+
+class ForwardProperty(object):
+    def __init__(self, target_member, property_name):
+        """
+        This is a small helper class that can be used to act as
+        a forwarding property to relay property setting/getting
+        to a member of the class it's installed on.
+
+        Typical use would be:
+
+            a = Foo()
+            a._b = Bar() # Bar has property baz
+
+            type(a).forward = ForwardProperty('_b', 'baz')
+
+            a.forward = 10 # equivalent to a._b.baz = 10
+
+        Note that this modifies the type Baz. Usage must thus be
+        limited to cases where this type modification is
+        acceptable.
+
+        :param target_member: Target member to forward to.
+        :param prop: Property of target to access.
+        """
+        self._target_member = target_member
+        self._prop = property_name
+
+    def __get__(self, instance, type=None):
+        """
+        This method forwards property read access on instance
+        to the member of instance that was selected in __init__.
+
+        :param instance: Instance of type.
+        :param type: Type.
+        :return: Attribute value of member property.
+        """
+        return getattr(getattr(instance, self._target_member), self._prop)
+
+    def __set__(self, instance, value):
+        """
+        This method forwards property write access on instance
+        to the member of instance that was selected in __init__.
+
+        :param instance: Instance of type.
+        :param value: Type.
+        """
+        setattr(getattr(instance, self._target_member), self._prop, value)
+
+
+class ForwardMethod(object):
+    def __init__(self, target, method):
+        self._target = target
+        self._method = method
+
+    def __call__(self, *args, **kwargs):
+        return getattr(self._target, self._method)(*args, **kwargs)
---
+++ plankton/core/utils.py
@@ -0,0 +1,114 @@
+#  -*- coding: utf-8 -*-
+# *********************************************************************
+# plankton - a library for creating hardware device simulators
+# Copyright (C) 2016 European Spallation Source ERIC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *********************************************************************
+
+import imp
+import os.path as osp
+from datetime import datetime
+from os import listdir
+
+
+def get_available_submodules(package, search_path=None):
+    """
+    This function returns a list of available submodules in a package.
+
+    :param package: Name of the package.
+    :param search_path: List of paths to search for package or None. Passed to imp.find_module
+    :return: Available submodules in package.
+    """
+    file_name, path, descriptor = imp.find_module(package, search_path)
+
+    submodule_candidates = [extract_module_name(osp.join(path, entry)) for entry in listdir(path)]
+
+    return [submodule for submodule in submodule_candidates if is_module(submodule, [path])]
+
+
+def extract_module_name(absolute_path):
+    """
+    This function tries to extract a valid module name from the basename of the supplied path.
+    If it's a directory, the directory name is returned, if it's a file, the file name
+    without extension is returned. If the basename starts with _ or it's a file with an
+    ending different from .py, the function returns None
+
+    :param absolute_path: Absolute path of something that might be a module.
+    :return: Module name or None.
+    """
+    base_name = osp.basename(osp.normpath(absolute_path))
+
+    # If the basename starts with _ it's probably __init__.py or __pycache__ or something internal.
+    # At the moment there seems to be no use case for those
+    if base_name.startswith('_'):
+        return None
+
+    # If it's a directory, there's nothing else to check, so it can be returned directly
+    if osp.isdir(absolute_path):
+        return base_name
+
+    module_name, extension = osp.splitext(base_name)
+
+    # If it's a file, it must have a .py ending
+    if extension == '.py':
+        return module_name
+
+    return None
+
+
+def is_module(module, paths):
+    """
+    Small helper function that returns True if module is a sub-module in package.
+
+    :param module: Name of the sub-module to check.
+    :param path: List of paths where the module is located.
+    :return: True if module is a sub-module of package.
+    """
+    try:
+        imp.find_module(module, paths)
+        return True
+    except (ImportError, TypeError):
+        return False
+
+
+def dict_strict_update(base_dict, update_dict):
+    """
+    This function updates base_dict with update_dict if and only if update_dict does not contain
+    keys that are not already in base_dict. It is essentially a more strict interpretation of the
+    term "updating" the dict.
+
+    If update_dict contains keys that are not in base_dict, a RuntimeError is raised.
+
+    :param base_dict: The dict that is to be updated. This dict is modified.
+    :param update_dict: The dict containing the new values.
+    """
+    additional_keys = set(update_dict.keys()) - set(base_dict.keys())
+    if len(additional_keys) > 0:
+        raise RuntimeError(
+            'The update dictionary contains keys that are not part of '
+            'the base dictionary: {}'.format(str(additional_keys)))
+
+    base_dict.update(update_dict)
+
+
+def seconds_since(start):
+    """
+    This is a small helper function that returns the elapsed seconds
+    since start using datetime.datetime.now().
+
+    :param start: Start time.
+    :return: Elapsed seconds since start time.
+    """
+    return (datetime.now() - start).total_seconds()
