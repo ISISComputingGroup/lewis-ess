0f4a63cef4ee89286c3c35968abdf71f3b871e56
Michael Wedel <michael.wedel@esss.se>
2016-10-27 16:29:05 +0200

    Remove additional space in coding comment

--- plankton-control.py
+++ plankton-control.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#  -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # *********************************************************************
 # plankton - a library for creating hardware device simulators
 # Copyright (C) 2016 European Spallation Source ERIC
