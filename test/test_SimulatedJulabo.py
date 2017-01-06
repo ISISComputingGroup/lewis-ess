215cbddf1c0ae258ab1b02c41c0617b9e6b9482b
Matt Clarke <matt.clarke@stfc.ac.uk>
2017-01-06 11:45:34 +0000

    Updated unit tests

--- test/test_SimulatedJulabo.py
+++ test/test_SimulatedJulabo.py
@@ -38,48 +38,47 @@ class TestSimulatedJulabo(unittest.TestCase):
             :param value: The new value.
         """
         setter = getattr(self.julabo_device, 'set_%s' % par_name)
-        getter = getattr(self.julabo_device, 'get_%s' % par_name)
         setter(value)
         self.go_forward_one_minute()
-        self.assertEqual(value, getter())
+        self.assertEqual(value, getattr(self.julabo_device, par_name))
 
     def test_default_construction(self):
         assertRaisesNothing(self, SimulatedJulabo)
 
     def test_changing_setpoint_changes_setpoint(self):
-        old_sp = self.julabo_device.get_set_point()
+        old_sp = self.julabo_device.set_point_temperature
 
         self.julabo_device.set_set_point(old_sp + 10.0)
 
-        self.assertEqual(old_sp + 10.0, self.julabo_device.get_set_point())
+        self.assertEqual(old_sp + 10.0, self.julabo_device.set_point_temperature)
 
     def test_on_initialisation_circulating_is_off(self):
-        self.assertEqual(0, self.julabo_device.get_circulating())
+        self.assertEqual(0, self.julabo_device.is_circulating)
 
     def test_on_stating_circulating_it_starts_circulating(self):
         self.julabo_device.set_circulating(1)
-        self.assertEqual(1, self.julabo_device.get_circulating())
+        self.assertEqual(1, self.julabo_device.is_circulating)
 
     def test_changing_setpoint_does_not_change_temperature_if_not_circulating(self):
-        old_sp = self.julabo_device.get_set_point()
-        old_t = self.julabo_device.get_bath_temperature()
+        old_sp = self.julabo_device.set_point_temperature
+        old_t = self.julabo_device.temperature
 
         self.julabo_device.set_set_point(old_sp + 10.0)
         self.go_forward_one_minute()
 
-        self.assertEqual(old_t, self.julabo_device.get_bath_temperature())
+        self.assertEqual(old_t, self.julabo_device.temperature)
 
     def test_changing_setpoint_does_change_temperature_if_circulating(self):
         self.julabo_device = SimulatedJulabo()
         self.julabo_device.process()  # Initialise
-        old_sp = self.julabo_device.get_set_point()
-        old_t = self.julabo_device.get_bath_temperature()
+        old_sp = self.julabo_device.set_point_temperature
+        old_t = self.julabo_device.temperature
 
         self.julabo_device.set_set_point(old_sp + 10.0)
         self.julabo_device.set_circulating(1)
         self.go_forward_one_minute()
 
-        self.assertNotEqual(old_t, self.julabo_device.get_bath_temperature())
+        self.assertNotEqual(old_t, self.julabo_device.temperature)
 
     def test_setting_external_p_sets_p(self):
         self.check_setting_values_works('external_p', 10)
