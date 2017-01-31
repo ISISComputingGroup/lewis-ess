3f377228851a62eb8ceba00af0711fae040ee968
Michael Wedel <michael.wedel@esss.se>
2017-01-31 10:49:09 +0100

    V20 Chopper Cascade

--- lewis/devices/v20_jcns_choppers/__init__.py
+++ lewis/devices/v20_jcns_choppers/__init__.py
@@ -20,6 +20,8 @@
 from lewis.core.utils import check_limits
 from lewis.devices.socket_device import SocketDevice
 from lewis.adapters.epics import EpicsAdapter, PV
+from lewis.adapters import ForwardProperty
+from collections import OrderedDict
 
 
 def ok_nok(str_val):
@@ -68,79 +70,107 @@ class JCNSChopperCascade(SocketDevice):
                                 zip(self.fields, elements[start_idx:end_idx])}
 
 
-class JCNSChopperCascadeEpicsInterface(EpicsAdapter):
-    pvs = {
-        'Factor': PV('factor', type='int'),
-        'Drive': PV('drive', type='enum', enums=['START', 'STOP']),
-        'DrivePower': PV('drive_power', type='enum', enums=['ON', 'OFF'], read_only=True),
-        'DriveTemp': PV('drive_temperature', read_only=True),
-        'Phase': PV('phase', read_only=True),
-        'Phase-SP': PV('phase_setpoint'),
-        'Direction': PV('direction', type='enum', enums=['CLOCK', 'ANTICLOCK'], read_only=True),
-        'Speed': PV('speed', read_only=True),
-        'Speed-SP': PV('speed_setpoint', read_only=True),
-        'Status': PV('status', type='int', read_only=True)
-    }
+class JCNSChopperEpicsInterface(object):
+    pvs = OrderedDict([
+        ('Factor', PV('factor', type='int')),
+        ('Drive', PV('drive', type='enum', enums=['START', 'STOP'])),
+        ('DrivePower', PV('drive_power', type='enum', enums=['ON', 'OFF'], read_only=True)),
+        ('DriveTemp', PV('drive_temperature', read_only=True)),
+        ('Phase', PV('phase', read_only=True)),
+        ('Phase-SP', PV('phase_setpoint')),
+        ('Direction', PV('direction', type='enum', enums=['CLOCK', 'ANTICLOCK'], read_only=True)),
+        ('Speed', PV('speed', read_only=True)),
+        ('Speed-SP', PV('speed_setpoint', read_only=True)),
+        ('Status', PV('status', type='int', read_only=True))
+    ])
+
+    pv_prefix = None
+
+    def __init__(self, device):
+        self._device = device
 
     @property
     def factor(self):
-        return self._device.state['C01']['FACT']
+        return self._device.state[self.pv_prefix]['FACT']
 
     @factor.setter
     @check_limits(1, 5)
     def factor(self, new_factor):
-        self._device.write('C01!;FACT!;%.2f' % new_factor)
+        self._device.write('%s!;FACT!;%.2f' % (self.pv_prefix, new_factor))
 
     @property
     def drive(self):
-        return self._device.state['C01']['SDRI']
+        return self._device.state[self.pv_prefix]['SDRI']
 
     @drive.setter
     def drive(self, new_state):
-        self._device.write('C01!;DRIV!;%s' % 'START' if new_state == 0 else 'STOP')
+        self._device.write(
+            '%s!;DRIV!;%s' % (self.pv_prefix, 'START' if new_state == 0 else 'STOP'))
 
     @property
     def drive_power(self):
-        return self._device.state['C01']['DRON']
+        return self._device.state[self.pv_prefix]['DRON']
 
     @property
     def phase(self):
-        return self._device.state['C01']['PHAS']
+        return self._device.state[self.pv_prefix]['PHAS']
 
     @property
     def phase_setpoint(self):
-        return self._device.state['C01']['SPHA']
+        return self._device.state[self.pv_prefix]['SPHA']
 
     @phase_setpoint.setter
     @check_limits(0, 360)
     def phase_setpoint(self, new_setpoint):
-        self._device.write('C01!;SPHA!;%.2f' % new_setpoint)
+        self._device.write('%s!;SPHA!;%.2f' % (self.pv_prefix, new_setpoint))
 
     @property
     def drive_temperature(self):
-        return self._device.state['C01']['DRIT']
+        return self._device.state[self.pv_prefix]['DRIT']
 
     @property
     def direction(self):
-        return self._device.state['C01']['RODI']
+        return self._device.state[self.pv_prefix]['RODI']
 
     @property
     def speed(self):
-        return self._device.state['C01']['SPEE']
+        return self._device.state[self.pv_prefix]['SPEE']
 
     @property
     def speed_setpoint(self):
-        return self._device.state['C01']['SSPE']
+        return self._device.state[self.pv_prefix]['SSPE']
 
     @property
     def status(self):
         ret = 0
         for field in self._device.fields:
             if field[1] == ok_nok:
-                ret = (ret << 1) | self._device.state['C01'][field]
+                ret = (ret << 1) | self._device.state[self.pv_prefix][field]
 
         return ret
 
+
+class JCNSChopperCascadeEpicsInterface(EpicsAdapter):
+    pvs = OrderedDict()
+
+    def __init__(self, device, arguments=None):
+        for name in ('C01', 'C02', 'C03', 'C04'):
+            chopper = JCNSChopperEpicsInterface(device)
+            chopper.pv_prefix = name
+
+            setattr(self, name, chopper)
+
+            pref = chopper.pv_prefix
+            for pv_name, pv in chopper.pvs.items():
+                new_prop = pref + '_' + pv.property
+                real_pv = pref + ':' + pv_name
+                self.pvs[real_pv] = PV(new_prop, pv.poll_interval,
+                                       pv.read_only, pv.meta_data_property, pv.doc,
+                                       **pv.config)
+
+                setattr(type(self), new_prop, ForwardProperty(name, pv.property, instance=self))
+
+        super(JCNSChopperCascadeEpicsInterface, self).__init__(device, arguments)
 
 
 setups = dict(
