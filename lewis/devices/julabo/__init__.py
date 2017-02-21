7cd66054ddc0f3cfa9fe287e468a962a7bf19edd
Michael Hart <michael.hart@stfc.ac.uk>
2017-02-21 10:31:16 +0000

    Removed unnecessary Device class import/export

--- lewis/devices/julabo/__init__.py
+++ lewis/devices/julabo/__init__.py
@@ -16,7 +16,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # *********************************************************************
-
-from .device import SimulatedJulabo
-
-__all__ = ['SimulatedJulabo']
